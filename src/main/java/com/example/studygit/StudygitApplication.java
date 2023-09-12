c53227c769cb414808714177a5325d17af4b5b0d
‘helloworld’ <213@qq.comgit config --global user.email 213@qq.com>
2023-09-12 21:48:52 +0800

    基础

---
+++ src/main/java/com/example/studygit/StudygitApplication.java
@@ -0,0 +1,13 @@
+package com.example.studygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudygitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studygit/StudygitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudygitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
